e04813c3eb8af3cea8633f902e36a98eed25597e
unknown <Shadabkhan023>
2025-02-19 22:32:57 +0530

    yes

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
+#!/usr/bin/env bash
+# Exit on error
 set -o errexit
 
+# Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
-python manage.py migrate 
 
-if [[$CREATE_SUPERUSER]] ;
-then 
-  python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
-fi
+# Convert static asset files
+python manage.py collectstatic --no-input
+
+# Apply any outstanding database migrations
+python manage.py migrate
